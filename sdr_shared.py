af857ea68eda5c14828a228eab73f08c085939de
akilacb117 <akilacb.117@gmail.com>
2026-09-17 19:12:47 +0530

    Add files via upload

--- sdr_shared.py
+++ sdr_shared.py
@@ -1,5 +1,3 @@
 import queue
-
-# Thread-safe IPC message queues between GUI and GNU Radio worker threads
-tx_queue = queue.Queue()
-rx_queue = queue.Queue()
+# This single global queue will be shared safely between GUI and GNU Radio
+MESSAGE_QUEUE = queue.Queue()
